f7197cf60bf94a72109511aeb5b4e48f0c2bbc47
sunilkarki <karkisunil1200@gmail.com>
2022-01-17 10:24:37 -0600

    rendering svg test

--- src/features/restaurants/components/restuarant-info.card.js
+++ src/features/restaurants/components/restuarant-info.card.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { SvgXml } from "react-native-svg";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -45,6 +47,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
+        <SvgXml xml={star} />
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
